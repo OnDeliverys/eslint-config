ff4f451c386372000502e8f25c7382a53cf80101
Matheus Santos <mat982005@gmail.com>
2024-06-08 22:49:30 -0300

    bump package.json version to 1.0.6

--- nest.js
+++ nest.js
@@ -29,12 +29,4 @@ module.exports = {
             }
         ]
     },
-    settings: {
-        "import/resolver": {
-            typescript: {
-                alwaysTryTypes: true,
-                project: "./tsconfig.json"
-            }
-        }
-    }
 }
