5155668155271c3a4faf11de806a2365affebac7
Matheus Santos <mat982005@gmail.com>
2024-06-09 01:00:29 -0300

    possible fix for nestjs

--- nest.js
+++ nest.js
@@ -1,8 +1,9 @@
+const path = require('path');
+
 module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: __dirname,
+        project: path.resolve(__dirname, './tsconfig.json'),
         sourceType: "module",
         ecmaVersion: 2020
     },
@@ -29,4 +30,4 @@ module.exports = {
             }
         ]
     },
-}
+};
